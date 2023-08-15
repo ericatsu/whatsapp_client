f6ecdfc51d2c7975e1f0f9ed283f14a3b748c17f
ericatsu <ekaatsu@st.ug.edu.gh>
2023-08-15 17:00:46 +0000

    init

---
+++ android/app/src/main/kotlin/com/ericatsu/whatsapp_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ericatsu.whatsapp_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String imageBlank = '${imagesPath}imageBlank.png';
+}
+
+class Palette {
+  static const Color primary = Color(0xFF0E0703);
+  static const Color background = Colors.white;
+  static const Color secondary = Color(0xFFEBD4D7);
+}
